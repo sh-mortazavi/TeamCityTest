c04618dc5efe593092ac06b9c5dab35c2c2d6166
sh-mortazavi <shmortazavi@live.com>
2015-06-06 16:55:46 +0430

    Create new project

---
+++ TeamCityAndAzureTest/TeamCityAndAzureTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityAndAzureTest.Startup))]
+namespace TeamCityAndAzureTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
